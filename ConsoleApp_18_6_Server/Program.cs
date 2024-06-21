1f52702125d006c16c22605a99b7b0e95c6560bf
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-21 19:22:31 +0300

    server

--- ConsoleApp_18_6_Server/Program.cs
+++ ConsoleApp_18_6_Server/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class ServerSync
+{
+    static void Main(string[] args)
+    {
+        System.Console.OutputEncoding = Encoding.Unicode;
+
+        // Створюємо серверний сокет
+        TcpListener server = new TcpListener(IPAddress.Any, 12345);
+        server.Start();
+        Console.WriteLine("Сервер запущено...");
+
+        while (true)
+        {
+            // Приймаємо підключення від клієнта
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+
+            // Читаємо дані від клієнта
+            byte[] buffer = new byte[256];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            string clientIP = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
+            Console.WriteLine($"О {DateTime.Now:HH:mm} від {clientIP} отримано рядок: {message}");
+
+            // Відправляємо відповідь клієнту
+            string response = "Привіт, клієнте!";
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseData, 0, responseData.Length);
+
+            // Закриваємо підключення
+            stream.Close();
+            client.Close();
+        }
+    }
+}
